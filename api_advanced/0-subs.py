52b47833d5f92e31e68b148ed79c1678e4385e12
g.mugisha4@alustudent.com <g.mugisha4@alustudent.com>
2026-02-23 21:25:43 +0000

    Fix User-Agent in 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyBot/1.0"}
+    headers = {"User-Agent": "linux:myredditapp:v1.0 (by /u/GabbyIT-pixel)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
