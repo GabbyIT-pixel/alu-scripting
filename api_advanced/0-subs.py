c08934050b559a719e921ab3d8c42f2ff710723c
g.mugisha4@alustudent.com <g.mugisha4@alustudent.com>
2026-02-23 20:43:00 +0000

    0-subs.py

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function that queries Reddit API and returns number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    return 0
